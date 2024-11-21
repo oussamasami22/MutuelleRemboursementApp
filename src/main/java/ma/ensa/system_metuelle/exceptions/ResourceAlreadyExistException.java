e639d595cd7d1e2897460860487d385a2e9a0e48
OUSSAMA ELG <oussama.elgoum@gmail.com>
2024-11-21 18:17:41 +0100

    exception

---
+++ src/main/java/ma/ensa/system_metuelle/exceptions/ResourceAlreadyExistException.java
@@ -0,0 +1,7 @@
+package ma.ensa.system_metuelle.exceptions;
+
+public class ResourceAlreadyExistException extends Exception {
+    public  ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ma/ensa/system_metuelle/processors/TraitementMappingProcessor.java
+++ src/main/java/ma/ensa/system_metuelle/processors/TraitementMappingProcessor.java
@@ -1,11 +1,21 @@
 package ma.ensa.system_metuelle.processors;
 
 import ma.ensa.system_metuelle.models.Dossier;
+import ma.ensa.system_metuelle.models.MedicamentReferentiel;
+import ma.ensa.system_metuelle.models.Traitement;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TraitementMappingProcessor implements ItemProcessor<Dossier,Dossier> {
+    @Autowired
+    private  IMedRefService medRefService;
     @Override
     public Dossier process(Dossier item) throws Exception {
-        return null;
-    }
+
+        for(Traitement traitement : item.getTraitements()){
+            MedicamentReferentiel ref=medRefService.getTraitement(traitement.getCodeBarre());
+            traitement.setMedicamentReferentiel(ref);
+            traitement.setExiste(ref != null);
+        }
+        return item;
 }
